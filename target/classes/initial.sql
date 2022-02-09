a227bb406adb0548e16b5c369cc8f0a59ae60ba6
MALossov <lshy1104@163.com>
2022-02-09 21:25:08 +0800

    delete target

--- target/classes/initial.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `userdata` (
-                            `Number` int NOT NULL,
-                            `UserName` varchar(45) NOT NULL,
-                            `Password` varchar(45) NOT NULL,
-                            `Email` varchar(45) NOT NULL,
-                            PRIMARY KEY (`Number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
